9b1c8a2bd1e545a21454309d883bf45622ba38f3
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-04-03 20:51:19 +0300

    cool

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const entry = document.getElementById('button');
 function addingEventListener() {
-}
+     entry.addEventListener('click', clicker);
+
+};
+
+function clicker(){
+    HTMLFormControlsCollection.log('clicked!');
+};
